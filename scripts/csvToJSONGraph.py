0c7488fdb4f5978ef81bdb845fbc6eda14ba84f3
macramole <leandrogarber@gmail.com>
2017-06-08 22:07:01 -0300

    by entrance

--- scripts/csvToJSONGraph.py
+++ scripts/csvToJSONGraph.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import json
 
 csv = open("./Lekagul Sensor Data.csv", "r")
@@ -45,7 +47,15 @@ for gateID, gate in enumerate(gateNames):
 
 linksSimple = {}
 
+# que tal si hacemos muchos grafos por cada entrada
+byEntrance = {}
+
 for key, car in trajectories.items():
+    entrance = car[0]["gate"]
+
+    if not entrance in byEntrance:
+        byEntrance[ entrance ] = {}
+
     for i in range(1,len(car)):
         info = car[i]
         prevInfo = car[i-1]
@@ -57,19 +67,25 @@ for key, car in trajectories.items():
 
         if not sourceTarget in linksSimple:
             linksSimple[sourceTarget] = 0
+        if not sourceTarget in byEntrance[ entrance ]:
+            byEntrance[ entrance ][sourceTarget] = 0
 
         linksSimple[sourceTarget] += 1
+        byEntrance[ entrance ][ sourceTarget ] += 1
 
-        jsonFinal["links"].append({
-            "source" : sourceID,
-            "target" : targetID,
-            "type" : info["type"],
-            "time" : info["time"],
-            "value" : 1
-        })
+        # descomentar esto para grafo completo
+        #
+        # jsonFinal["links"].append({
+        #     "source" : sourceID,
+        #     "target" : targetID,
+        #     "type" : info["type"],
+        #     "time" : info["time"],
+        #     "value" : 1
+        # })
 
-with open ("./sensorData.graph.json", "w") as j:
-    json.dump( jsonFinal , j )
+# with open ("./sensorData.graph.json", "w") as j:
+#     json.dump( jsonFinal , j )
+#     print("hice sensorData.graph.json")
 
 jsonSimple = {
     "nodes" : jsonFinal["nodes"],
@@ -88,3 +104,28 @@ for sourceTarget, value in linksSimple.items():
 
 with open ("./sensorData.graph.simple.json", "w") as j:
     json.dump( jsonSimple , j )
+    print("hice sensorData.graph.simple.json")
+
+
+jsonSimpleByEntrance = {
+    "nodes" : jsonFinal["nodes"],
+    "entrances" : {}
+}
+
+for entranceName, sourceTargets in byEntrance.items():
+    jsonSimpleByEntrance["entrances"][entranceName] = []
+    links = jsonSimpleByEntrance["entrances"][entranceName]
+
+    for sourceTarget, value in sourceTargets.items():
+        arrSourceTarget = sourceTarget.split("-")
+        source = int(arrSourceTarget[0])
+        target = int(arrSourceTarget[1])
+        links.append({
+            "source" : source,
+            "target" : target,
+            "value" : value
+        })
+
+with open ("./sensorData.graph.simple.byEntrance.json", "w") as j:
+    json.dump( jsonSimpleByEntrance , j )
+    print("hice sensorData.graph.simple.byEntrance.json")
